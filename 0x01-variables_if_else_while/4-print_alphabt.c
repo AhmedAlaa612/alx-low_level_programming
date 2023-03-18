d5a43312039149ad17f0773bccae24a6534c1b31
ED <ahmedalaa555r@gmail.com>
2023-03-18 14:49:43 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main funciton
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
