cb84119f290e7feb1c59be74291094237708b134
ED <ahmedalaa555r@gmail.com>
2023-04-02 13:23:55 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: string 
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int len = 0, i;
+
+	while (s[len] != '\0')
+		len++;
+	for (i = 0; i < len / 2; i++)
+	{
+		len--;
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+	}
+}
