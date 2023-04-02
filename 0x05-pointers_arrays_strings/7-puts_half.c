adb661a22ac17e7d4ea322950163cc602d1b0886
ED <ahmedalaa555r@gmail.com>
2023-04-02 13:51:35 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - preint second half of the string
+ * @str: the string
+ */
+void puts_half(char *str)
+{
+	int i, len = 0, n;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
