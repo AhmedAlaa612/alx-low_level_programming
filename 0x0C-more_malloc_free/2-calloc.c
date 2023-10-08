94d5928ae2e6ae45896374144a8648eaa8a4307e
ED <ahmedalaa555r@gmail.com>
2023-10-08 00:50:41 -0700

    more malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,7 +30,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	m = malloc(size * nmemb);
+	m = malloc(sizeof(int) * nmemb);
 
 	if (m == NULL)
 		return (NULL);
