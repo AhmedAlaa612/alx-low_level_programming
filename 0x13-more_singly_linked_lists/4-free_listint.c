c2d701a2bce49d0695db9e4b005536be6f971eea
ED <ahmedalaa555r@gmail.com>
2023-10-25 00:59:21 +0200

    /linkedlists/

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the first element of the list
+ * Return: void
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
