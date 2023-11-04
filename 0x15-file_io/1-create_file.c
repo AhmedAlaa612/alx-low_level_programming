ed5b231be106f1921d52bebb46b36faf423d90d5
ED <ahmedalaa555r@gmail.com>
2023-11-04 02:06:20 +0200

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int _strlen(char *s)
  * create_file - creates a file
  * @filename: file name
  * @text_content: text to write
- * Return: 1 or 0
+ * Return: 1 or -1
  */
 int create_file(const char *filename, char *text_content)
 {
